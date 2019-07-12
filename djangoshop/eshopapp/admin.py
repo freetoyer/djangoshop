39432f1f40411c5ee71dc46c73fad8118e0da68f
andrey <andrey_89-89@mail.ru>
2019-07-12 22:07:46 +0300

    add model Product

--- djangoshop/eshopapp/admin.py
+++ djangoshop/eshopapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from eshopapp.models import Category, Brand
+from eshopapp.models import Category, Brand, Product
 
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(Product)
--- djangoshop/eshopapp/models.py
+++ djangoshop/eshopapp/models.py
@@ -15,3 +15,19 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Category)
+    brand = models.ForeignKey(Brand)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
