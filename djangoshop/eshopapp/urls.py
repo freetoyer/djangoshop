9c0e022a55570983bba814e40559c4b48d04935c
andrey <andrey_89-89@mail.ru>
2019-07-16 17:56:49 +0300

    correct margin-top in base.html

--- djangoshop/eshopapp/urls.py
+++ djangoshop/eshopapp/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from eshopapp.views import base_view, category_view, product_view
 
 urlpatterns = [
-    url(r'^category/(?P<category_slug>(\w)+)/$', category_view, name='category_detail'),
-    url(r'^product/(?P<product_slug>(\w)+)/$', product_view, name='product_detail'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', category_view, name='category_detail'),
+    url(r'^product/(?P<product_slug>[-\w]+)/$', product_view, name='product_detail'),
     url(r'^$', base_view, name='base'),
 ]
 
