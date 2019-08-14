7d523ea82bba27f208028707d4757aeedf61a118
and <andrey_89-89@mail.ru>
2019-08-14 21:20:52 +0300

    items_in_order in admin

--- djangoshop/eshopapp/admin.py
+++ djangoshop/eshopapp/admin.py
@@ -8,8 +8,15 @@ make_paid.short_description = "Пометить как оплаченные"
 
 class OrderAdmin(admin.ModelAdmin):
     list_filter = ['status']
+    list_display = ['id', 'items_in_order']
     actions = [make_paid]
     readonly_fields = ('user', 'items', 'total',)
+
+    def items_in_order(self, obj):
+        items_in_order = '<br>'.join(['Товар - {0} | Кол-во - {1}'.format(item.product.title, item.qty) for item in obj.items.items.all()])
+        return items_in_order
+
+    items_in_order.allow_tags = True
 
 admin.site.register(Category)
 admin.site.register(Brand)
